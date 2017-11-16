40c894ee06f14b6e445aadebfb299f5da093a43e
EmeraldJelly <nickb122717@icloud.com>
2017-11-16 17:43:36 -0500

    Finished Desgin
    Completed the Design layout and Basic Segues.

---
+++ Swoosh/Swoosh/Views/RotatingView.swift
@@ -0,0 +1,15 @@
+//
+//  RotatingView.swift
+//  Swoosh
+//
+//  Created by Nicholas Brewster on 11/15/17.
+//  Copyright © 2017 Nicholas Brewster. All rights reserved.
+//
+
+import UIKit
+
+class RotatingView: UIStackView {
+    
+    
+
+}
