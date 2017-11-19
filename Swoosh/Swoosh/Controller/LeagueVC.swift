b926aa058c6207b56d1d0fe32044d55b0db42c2a
EmeraldJelly <nickb122717@icloud.com>
2017-11-19 17:50:02 -0500

    League Selection
    + Model for Player
    + Struct for Player
    + Method for League Selection

--- Swoosh/Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -9,15 +9,39 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
-   
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        player = Player()
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "leagueToSkillSegue", sender: self)
     }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+       selectLeague(leagueType: "Mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "Womens")
+
+    }
+    
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true;
+    }
+    
 }
